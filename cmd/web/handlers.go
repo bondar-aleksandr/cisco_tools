83807782c2456bf3d4a94b8ece09dd5eef24e07e
abondar <aleksand.bondar.1985@gmail.com>
2023-08-07 17:01:50 +0300

    basic functionality is ready

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,25 +1,22 @@
 package main
 
 import (
-	// "fmt"
-	"io/ioutil"
+	"bytes"
+	"fmt"
+	"io"
 	"net/http"
 	"os"
-	// "strings"
-
+	"strconv"
 	"github.com/bondar-aleksandr/ios-config-parsing/parser"
 	log "github.com/sirupsen/logrus"
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-	// fmt.Fprintf(w, "home")
-
 	data := &templateData{}
 	app.render(w, http.StatusOK, "home.tmpl", data)
 }
 
 func (app *application) configParser(w http.ResponseWriter, r *http.Request) {
-	// fmt.Fprintf(w, "config-parser page")
 	data := &templateData{}
 	app.render(w, http.StatusOK, "config-parser.tmpl", data)
 }
@@ -37,50 +34,52 @@ func (app *application) configUpload(w http.ResponseWriter, r *http.Request) {
     defer file.Close()
 	log.Infof("Uploaded File: %+v\n", fileHeader.Filename)
 	log.Infof("File Size: %+v\n", fileHeader.Size)
-	log.Infof("MIME Header: %+v\n", fileHeader.Header)
+	log.Infof("MIME Header: %+v\n", fileHeader.Header.Values("Content-Type"))
 
-	tempFile, err := os.CreateTemp("./temp", "config-*.txt")
+    fileBytes, err := io.ReadAll(file)
+    if err != nil {
+        app.serverError(w, err)
+		return
+    }
+    
+	//processing part
+	osFamily := r.FormValue("osFamily")
+	outputFormat := r.FormValue("outputFormat")
+	log.Infof("Selected the following: OS family - %s, Outputformat - %s", osFamily, outputFormat)
+
+	buf := new(bytes.Buffer)
+	buf.Write(fileBytes)
+
+	interface_map := parser.Parsing(buf, osFamily)
+
+	tempFile, err := os.CreateTemp("./temp", fmt.Sprintf("output-*.%s", outputFormat))
 	if err != nil {
 		log.Error(err)
 		app.serverError(w, err)
 		return
 	}
-	
+
+	if outputFormat == "csv" {
+		interface_map.ToCSV(tempFile)
+	} else if outputFormat == "json" {
+		interface_map.ToJSON(tempFile)
+	}
+	log.Infof("Saved as %s", tempFile.Name())
+	tempFile.Close()
+
 	// clean up
 	defer func() {
-        err := os.Remove(tempFile.Name())
-        if err != nil {
+		err = os.Remove(tempFile.Name())
+		if err != nil {
 			log.Error(err)
-            app.serverError(w, err)
         }
     }()
 
-    // read all of the contents of our uploaded file into a byte array
-    fileBytes, err := ioutil.ReadAll(file)
-    if err != nil {
-        app.serverError(w, err)
-		return
-    }
-    // write this byte array to our temporary file
-    tempFile.Write(fileBytes)
+	//response to client
+	w.Header().Set("Content-Disposition", "attachment; filename="+strconv.Quote(tempFile.Name()))
+	w.Header().Set("Content-Type", "application/octet-stream")
+	http.ServeFile(w, r, tempFile.Name())
 
-	//TODO: processing part
-
-	osFamily := r.FormValue("osFamily")
-	outputFormat := r.FormValue("outputFormat")
-	log.Infof("Selected the following: OS family - %s, Outputformat - %s", osFamily, outputFormat)
-
-	interface_map := parser.Parsing(tempFile, osFamily)
-	if outputFormat == "csv" {
-		parser.ToCSV(interface_map, "./csv-output.csv")
-	} else if outputFormat == "json" {
-		interface_map.ToJSON("./json-output.json")
-	}
-
-	//end of processing
-	tempFile.Close()
-
-    // return that we have successfully uploaded our file!
-	data := &templateData{}
-	app.render(w, http.StatusAccepted, "config-upload.tmpl", data)
+	// data := &templateData{}
+	// app.render(w, http.StatusAccepted, "config-upload.tmpl", data)
 }
