ca20cb02f5a9151879ba6a28ce88097af608140c
abondar <aleksand.bondar.1985@gmail.com>
2023-08-08 20:12:17 +0300

    config yml parsing added

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,12 +1,26 @@
 package main
 
 import (
-	"net/http"
-	log "github.com/sirupsen/logrus"
-	// "os"
-	"time"
+	"fmt"
 	"html/template"
+	"net/http"
+	"os"
+	"time"
+
+	log "github.com/sirupsen/logrus"
+	"gopkg.in/yaml.v3"
 )
+
+type config struct {
+	Server struct {
+		Host string
+		Port string
+		ReadTimeout int
+		WriteTimeout int
+		IdleTimeout int
+		maxUpload int
+	}
+}
 
 type application struct {
 	// infoLog *log.Logger
@@ -16,12 +30,14 @@ type application struct {
 	templateCache map[string]*template.Template
 	// formDecoder *form.Decoder
 	// sessionManager *scs.SessionManager
+	config config
 }
 
-const PORT = ":4000"
+var appConfig config
 
 func main() {
-
+	readConfig(&appConfig)
+	
 	templateCache, err := newTemplateCache()
 	if err != nil {
         log.Fatal(err)
@@ -29,18 +45,33 @@ func main() {
 
 	app := &application{
 		templateCache: templateCache,
+		config: appConfig,
 	}
 	
 	srv := &http.Server{
-		Addr: PORT,
+		Addr: fmt.Sprintf("%s:%s", app.config.Server.Host, app.config.Server.Port),
 		Handler: app.routes(),
-		IdleTimeout: time.Minute,
-		ReadTimeout: 5 * time.Second,
-		WriteTimeout: 10 * time.Second,
+		IdleTimeout: time.Duration(app.config.Server.IdleTimeout) * time.Second,
+		ReadTimeout: time.Duration(app.config.Server.ReadTimeout) * time.Second,
+		WriteTimeout: time.Duration(app.config.Server.WriteTimeout) * time.Second,
 	}
 
-	log.Infof("starting server on port %s...\n", PORT)
+	log.Infof("starting server on port %s...\n", app.config.Server.Port)
 
 	err = srv.ListenAndServe()
 	log.Fatal(err)
+}
+
+func readConfig(cfg *config) {
+	f, err := os.Open("./config/config.yml")
+	if err != nil {
+		log.Fatalf("Cannot read app config file because of: %s", err)
+	}
+	defer f.Close()
+
+	decoder := yaml.NewDecoder(f)
+	err = decoder.Decode(cfg)
+	if err != nil {
+		log.Fatalf("Cannot parse app config file because of: %s", err)
+	}
 }
