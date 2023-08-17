fba5dac576843f8c83410489a93c96dbd6159288
abondar <aleksand.bondar.1985@gmail.com>
2023-08-17 16:53:22 +0300

    test for ConfigUpload handler

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -1,32 +1,74 @@
 package main
 
 import (
-	"net/http"
-	"testing"
+	"bytes"
 	"github.com/bondar-aleksandr/ios-config-parsing/internal/assert"
+	"io"
+	"mime/multipart"
+	"net/http"
+	"os"
+	"testing"
+	// "net/url"
 )
 
 func Test_Home_Pages(t *testing.T) {
-	
+
 	app := newTestApplication(t)
 	ts := newTestServer(t, app.routes())
 	defer ts.Close()
 
-	data := []struct{
-		name string
-		pageURL string
+	data := []struct {
+		name       string
+		pageURL    string
 		statusCode int
-		body string
+		body       string
 	}{
 		{"Homepage", "/", http.StatusOK, "<h2>List of apps available</h2>"},
 		{"ConfigParserHome", "/config-parser", http.StatusOK, "This app parses text configuration IOS/NXOS file"},
 	}
 
 	for _, val := range data {
-		t.Run(val.name, func(t *testing.T){
+		t.Run(val.name, func(t *testing.T) {
 			code, _, body := ts.get(t, val.pageURL)
 			assert.Equal(t, code, val.statusCode)
 			assert.StringContains(t, body, val.body)
 		})
+	}
+}
+
+func Test_config_Upload(t *testing.T) {
+	app := newTestApplication(t)
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
+
+	_, _, body := ts.get(t, "/config-parser")
+	validCSRFToken := extractCSRFToken(t, body)
+	t.Logf("CSRF token is: %q", validCSRFToken)
+
+	mbody := new(bytes.Buffer)
+
+	var mw = *multipart.NewWriter(mbody)
+	_ = mw.WriteField("osFamily", "ios")
+	_ = mw.WriteField("outputFormat", "csv")
+	_ = mw.WriteField("csrf_token", validCSRFToken)
+
+	file, err := os.Open("./../../parser/test_data/ASR-P.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	w, err := mw.CreateFormFile("configFile", file.Name())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if _, err := io.Copy(w, file); err != nil {
+		t.Fatal(err)
+	}
+	mw.Close()
+
+	code, _, _ := ts.post(t, "/config-parser/upload", mw.FormDataContentType(), mbody)
+	if code != http.StatusOK {
+		t.Errorf("wrong status code")
 	}
 }
--- cmd/web/testutils_test.go
+++ cmd/web/testutils_test.go
@@ -1,69 +1,111 @@
 package main
 
 import (
-    "bytes"
-    "io"
-    // "log"
-    "net/http"
-    "net/http/httptest"
-    "testing"
-	"net/http/cookiejar"
+	"bytes"
 	"github.com/alexedwards/scs/v2"
+	"html"
+	"io"
+	// "log"
+	"net/http"
+	"net/http/cookiejar"
+	"net/http/httptest"
+	"regexp"
+	"testing"
 )
+
+var csrfTokenRX = regexp.MustCompile(`<input type='hidden' name='csrf_token' value='(.+)'>`)
+
+func extractCSRFToken(t *testing.T, body string) string {
+	// Use the FindStringSubmatch method to extract the token from the HTML body.
+	// Note that this returns an array with the entire matched pattern in the
+	// first position, and the values of any captured data in the subsequent
+	// positions.
+	matches := csrfTokenRX.FindStringSubmatch(body)
+	if len(matches) < 2 {
+		t.Fatal("no csrf token found in body")
+	}
+	return html.UnescapeString(string(matches[1]))
+}
 
 // Create a newTestApplication helper which returns an instance of our
 // application struct containing mocked dependencies.
 func newTestApplication(t *testing.T) *application {
-    pathToTemplates = "./../../ui/html/"
+	pathToTemplates = "./../../ui/html/"
+	configPath = "./../../config/config.yml"
+	tempDir = "./../../temp"
 	tc, _ := newTemplateCache()
 
 	sessionManager := scs.New()
 	sessionManager.Cookie.Persist = false
 
+	readConfig(&appConfig)
+
 	app := application{
-		templateCache: tc,
+		templateCache:  tc,
 		sessionManager: sessionManager,
+		config:         &appConfig,
 	}
 	return &app
 }
 
 // Define a custom testServer type which embeds a httptest.Server instance.
 type testServer struct {
-    *httptest.Server
+	*httptest.Server
 }
 
 // Create a newTestServer helper which initalizes and returns a new instance
 // of our custom testServer type.
 func newTestServer(t *testing.T, h http.Handler) *testServer {
-    ts := httptest.NewServer(h)
+	ts := httptest.NewServer(h)
 
 	// Initialize a new cookie jar.
 	jar, err := cookiejar.New(nil)
 	if err != nil {
 		t.Fatal(err)
 	}
-    // Add the cookie jar to the test server client. Any response cookies will
-    // now be stored and sent with subsequent requests when using this client.
-    ts.Client().Jar = jar
+	// Add the cookie jar to the test server client. Any response cookies will
+	// now be stored and sent with subsequent requests when using this client.
+	ts.Client().Jar = jar
 
-    return &testServer{ts}
+	return &testServer{ts}
 }
 
 // Implement a get() method on our custom testServer type. This makes a GET
-// request to a given url path using the test server client, and returns the 
+// request to a given url path using the test server client, and returns the
 // response status code, headers and body.
 func (ts *testServer) get(t *testing.T, urlPath string) (int, http.Header, string) {
-    rs, err := ts.Client().Get(ts.URL + urlPath)
-    if err != nil {
-        t.Fatal(err)
-    }
+	rs, err := ts.Client().Get(ts.URL + urlPath)
+	if err != nil {
+		t.Fatal(err)
+	}
 
-    defer rs.Body.Close()
-    body, err := io.ReadAll(rs.Body)
-    if err != nil {
-        t.Fatal(err)
-    }
-    bytes.TrimSpace(body)
+	defer rs.Body.Close()
+	body, err := io.ReadAll(rs.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	bytes.TrimSpace(body)
 
-    return rs.StatusCode, rs.Header, string(body)
+	return rs.StatusCode, rs.Header, string(body)
+}
+
+// Implement a post() method on our custom testServer type. This makes a POST
+// request to a given url path using the test server client, and returns the
+// response status code, headers and body.
+func (ts *testServer) post(t *testing.T, urlPath string, ct string, b io.Reader) (int, http.Header, string) {
+	rs, err := ts.Client().Post(ts.URL+urlPath, ct, b)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Read the response body from the test server.
+	defer rs.Body.Close()
+	body, err := io.ReadAll(rs.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	bytes.TrimSpace(body)
+
+	// Return the response status, headers and body.
+	return rs.StatusCode, rs.Header, string(body)
 }
