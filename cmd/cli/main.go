4090bc66b29a03594475588613dc6fdf958fda54
abondar <aleksand.bondar.1985@gmail.com>
2023-09-18 19:13:26 +0300

    improvements is being made...

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,31 +1,46 @@
 package main
 
 import (
-	"github.com/bondar-aleksandr/cisco_parser"
 	"flag"
-	log "github.com/sirupsen/logrus"
+	"log"
 	"os"
+	"time"
+	"github.com/bondar-aleksandr/cisco_parser"
+)
+
+var (
+	InfoLogger  *log.Logger = log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
+	WarnLogger *log.Logger = log.New(os.Stdout, "WARNING: ", log.Ldate|log.Ltime|log.Lshortfile)
+	ErrorLogger *log.Logger = log.New(os.Stderr, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
 )
 
 func main() {
+
+	start := time.Now()
+	InfoLogger.Println("Starting...")
+
 	var iFileName = flag.String("i", "", "input configuration file to parse data from")
 	var oFileName = flag.String("o", "", "output csv file, default is input filename with .csv extension")
 	var devtype = flag.String("t", "ios", "cisco OS family, possible values are ios, nxos. Default is ios")
 	var jsonOut = flag.Bool("j", false, "Whether JSON file needed. Default is false")
 
+	if len(os.Args) < 2 {
+		ErrorLogger.Fatalf("No input data provided, use -h flag for help. Exiting...")
+	}
 	flag.Parse()
-	log.Infof(`Program started, got the following parameters: input file: %s, output file: %s,
-			device type: %s, JSON output: %v`, *iFileName, *oFileName, *devtype, *jsonOut)
+	//TODO: panic recovery if there are problems with iFile
+
+	InfoLogger.Printf("Program started, got the following parameters:\ninput file: %s\noutput file: %s\ndevice type: %s\nJSON output: %v\n", *iFileName, *oFileName, *devtype, *jsonOut)
 
 	iFile, err := os.Open(*iFileName)
 	if err != nil {
-		log.Fatalf("Can not open file %s because of: %q", iFile.Name(), err)
+		ErrorLogger.Fatalf("Can not open file %s because of: %q", iFile.Name(), err)
 	}
 	defer iFile.Close()
 
 	interface_map, err := cisco_parser.ParseInterfaces(iFile, *devtype)
 	if err != nil {
-		log.Fatal(err)
+		ErrorLogger.Fatal(err)
 	}	
 
 	if *oFileName == "" {
@@ -34,20 +49,21 @@ func main() {
 
 	oFile, err := os.Create(*oFileName)
 	if err != nil {
-		log.Fatalf("Error in writing csv data to file %s because of: %q", oFile.Name(), err)
+		ErrorLogger.Fatalf("Error in writing csv data to file %s because of: %q", oFile.Name(), err)
 	}
 	defer oFile.Close()
 	interface_map.ToCSV(oFile)
-	log.Infof("Saved to %s", oFile.Name())
+	InfoLogger.Printf("Saved to %s\n", oFile.Name())
 
 	if *jsonOut {		
 		jsonFileName := cisco_parser.FileExtReplace(*oFileName, "json")
 		jsonFile, err := os.Create(jsonFileName)
 		if err != nil {
-			log.Fatalf("Error in writing json data to file %s because of: %q", jsonFile.Name(), err)
+			ErrorLogger.Fatalf("Error in writing json data to file %s because of: %q", jsonFile.Name(), err)
 		}
 		defer jsonFile.Close()
 		interface_map.ToJSON(jsonFile)
-		log.Infof("Saved to %s", jsonFileName)
+		InfoLogger.Printf("Saved to %s\n", jsonFileName)
 	}
+	InfoLogger.Printf("Finished! Time taken: %s\n", time.Since(start))
 }
